028571749caaa10832a7fdc2f3b18ab1c337717e
phong.dao <phong.dao@saigontechnology.com>
2023-01-07 10:20:15 +0700

    Update clickable image

--- st_clickable_images/__init__.py
+++ st_clickable_images/__init__.py
@@ -5,27 +5,26 @@ _RELEASE = True
 
 if not _RELEASE:
     _component_func = components.declare_component(
-        "clickable_images", url="http://localhost:3001",
+        "clickable_images",
+        url="http://localhost:3001",
     )
 else:
     parent_dir = os.path.dirname(os.path.abspath(__file__))
     build_dir = os.path.join(parent_dir, "frontend/build")
-    _component_func = components.declare_component(
-        "st_clickable_images", path=build_dir
-    )
+    _component_func = components.declare_component("st_clickable_images", path=build_dir)
 
 
-def clickable_images(paths, titles=[], div_style={}, img_style={}, key=None):
-    """Display one or several images that can be clicked on".
+def clickable_images(paths, title: str = "", div_style={}, img_style={}, key=None):
+    """Create a new instance of "my_component".
 
     Parameters
     ----------
     paths: list
         The list of URLS of the images
-    
-    titles: list
+
+    title: list
         The (optional) titles of the images
-    
+
     div_style: dict
         A dict with the CSS property/value pairs for the div container
 
@@ -45,30 +44,11 @@ def clickable_images(paths, titles=[], div_style={}, img_style={}, key=None):
     """
     component_value = _component_func(
         paths=paths,
-        titles=titles,
+        titles=title,
         div_style=div_style,
         img_style=img_style,
         key=key,
-        default=-1,
+        default=False,
     )
 
     return component_value
-
-
-if not _RELEASE:
-    import streamlit as st
-
-    clicked = clickable_images(
-        [
-            "https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=700",
-            "https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=700",
-            "https://images.unsplash.com/photo-1582550945154-66ea8fff25e1?w=700",
-            "https://images.unsplash.com/photo-1591797442444-039f23ddcc14?w=700",
-            "https://images.unsplash.com/photo-1518727818782-ed5341dbd476?w=700",
-        ],
-        titles=[f"Image #{str(i)}" for i in range(5)],
-        div_style={"display": "flex", "justify-content": "center", "flex-wrap": "wrap"},
-        img_style={"margin": "5px", "height": "200px"},
-    )
-
-    st.markdown(f"Image #{clicked} clicked" if clicked > -1 else "No image clicked")
--- st_clickable_images/frontend/src/index.tsx
+++ st_clickable_images/frontend/src/index.tsx
@@ -16,26 +16,37 @@ function onRender(event: Event): void {
   }
 
   // Add and style all images
-  let imagesLoaded = 0
-  for (let i = 0; i < data.args["paths"].length; i++) {
-    let img = div.appendChild(document.createElement("img"))
-    for (let key in data.args["img_style"]) {
-      img.style[key as any] = data.args["img_style"][key]
-    }
-    img.src = data.args["paths"][i]
-    if (data.args["titles"].length > i) {
-      img.title = data.args["titles"][i]
-    }
-    img.onclick = function (): void {
-      Streamlit.setComponentValue(i)
-    }
-    // eslint-disable-next-line
-    img.onload = function (): void {
-      imagesLoaded++
-      if (imagesLoaded === data.args["paths"].length) {
-        Streamlit.setFrameHeight()
-      }
-    }
+  // for (let i = 0; i < data.args["paths"].length; i++) {
+  //   let img = div.appendChild(document.createElement("img"))
+  //   for (let key in data.args["img_style"]) {
+  //     img.style[key as any] = data.args["img_style"][key]
+  //   }
+  //   img.src = data.args["paths"][i]
+  //   if (data.args["titles"].length > i) {
+  //     img.title = data.args["titles"][i]
+  //   }
+  //   img.onclick = function (): void {
+  //     Streamlit.setComponentValue(i)
+  //   }
+  //   // eslint-disable-next-line
+  //   img.onload = function (): void {
+  //     imagesLoaded++
+  //     if (imagesLoaded === data.args["paths"].length) {
+  //       Streamlit.setFrameHeight()
+  //     }
+  //   }
+  // }
+  let img = div.appendChild(document.createElement("img"))
+  for (let key in data.args["img_style"]) {
+    img.style[key as any] = data.args["img_style"][key]
+  }
+  img.src = data.args["paths"]
+  img.title = data.args["title"]
+  img.onclick = function (): void {
+    Streamlit.setComponentValue(true)
+  }
+  img.onload = function (): void {
+    Streamlit.setFrameHeight()
   }
 }
 
